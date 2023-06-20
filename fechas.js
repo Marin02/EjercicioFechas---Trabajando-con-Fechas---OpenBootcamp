54176f6aedf15d1c06fd7f11bae54a7c15f25139
Juan Felipe Marin <102315395+Marin02@users.noreply.github.com>
2023-06-20 18:52:41 -0500

    Uploaded.

---
+++ fechas.js
@@ -0,0 +1,12 @@
+const fechaHoy = new Date();
+console.log(fechaHoy)
+
+const fechaNacimiento = new Date(2004, 1, 2);
+console.log(fechaNacimiento)
+
+const comparacionFechas = fechaHoy > fechaNacimiento;
+console.log(comparacionFechas)
+
+let dia = 2;
+let mes = 1;
+let año = 2004;
